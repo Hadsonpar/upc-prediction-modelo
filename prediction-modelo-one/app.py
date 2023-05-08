388f56358f5df400eae9e21377920d2c4d4e81d2
Hadsonpar <hadson1@gmail.com>
2023-05-08 07:24:02 -0500

    code about prediction modelo one and two

--- prediction-modelo-one/app.py
+++ prediction-modelo-one/app.py
@@ -1,27 +1,202 @@
 from fastapi import FastAPI
-from typing import Union
+# Celda #1 - librerias
+from typing import Union, List
+from pydantic import BaseModel
+import numpy as np
+import matplotlib.pyplot as plt
+from utils import *
+plt.style.use('./deeplearning.mplstyle')
+np.set_printoptions(precision=2)
+import json
+
+from utils_multi import * 
+
+# Celda #2 - Dataset
+#x_train, y_train, z_train = load_data(1)
+
+# Celda #6: Assign value the parameters w (pezo prediccion) / b (filtro inicial)
+w, b = weight_bias()
+#w = 1.12
+#b = 30
+
+class Prediction(BaseModel):    
+    inmueble: int
+    size: str
+    price: str
+    recommended: bool
+    location: str
+
+class Predictions(BaseModel): 
+    predictions: List[Prediction]
+
+# Definir bloque de vistas de registros
+def price_index(sizemts, x_train):    
+    xm2_train = np.array(x_train)
+    difference_array = np.absolute(xm2_train - sizemts)    
+    index = difference_array.argmin()    
+    return index
+
+
 
 app = FastAPI()
 
-@app.get("/price-model-land/{size}/{price}/{location}")
-async def read_calculate(size: str, price: str, location: Union[int, None] = None):
+@app.get("/price_model/{sizemts}/{location}/{priceuser}")
+def read_calculate(sizemts: int, location: str, priceuser: int, locariouser: Union[str, None] = None):
+    x_train, y_train, z_train = load_data(location)
+    xm2 = np.array(x_train)    
+    indexm2 = price_index(sizemts, xm2)  
+    nro = 0
+    valuemessage = ''
 
-    if location == 1:
-        locations = 'Cercado de Lima'
-    elif location == 2:
-        locations = 'Ancón'
-    elif location == 3:
-        locations = 'Ate'
-    elif location == 4:
-        locations = 'Barranco'
-    elif location == 5:
-        locations = 'Breña'
-    elif location == 6:
-        locations = 'Carabayllo'
+    data = {}
+    data['predictions'] = []   
+
+    if (sizemts < 12):
+            valuemessage = 'minimo 12 m^2'
+    elif (sizemts > 304):
+        valuemessage = 'maximo 304 m^2'
     else:
-        locations = 'Sin Ubicación'
+        for i in range(indexm2, indexm2+3):    
+            nro = nro + 1        
+            
+            #if (i == price_index(sizemts, x_train) and nro == 1):                                              
+            data['predictions'].append({
+                'inmueble': nro,
+                'size': x_train[i],
+                #'price': f"$ {format(w * x_train[i] + b,'0,.2f')}",
+                'price': f"$ {format(y_train[i] + b,'0,.2f')}",
+                'recommended': True,
+                'location': read_location(z_train[i])})
 
-    return {"size": size, "price": price, "location": locations}
+            valuemessage = 'completed process'
+            
+        """
+        elif(i != price_index(sizemts, x_train) and nro == 2):                                              
+            data['predictions'].append({
+                'inmueble': nro,
+                'size': x_train[i],
+                'price': f"$ {format(w * x_train[i] + b,'0,.2f')}",
+                'recommended': False,
+                'location': read_location(z_train[i])})
+            
+        elif(i != price_index(sizemts, x_train) and nro == 3):                       
+            data['predictions'].append({
+                 'inmueble': nro,
+                'size': x_train[i],
+                'price': f"$ {format(w * x_train[i] + b,'0,.2f')}",
+                'recommended': False,
+                'location': read_location(z_train[i])})
+        """ 
+    with open('modelorl.json', 'w') as file:
+        json.dump(data, file, indent=4)  
+    return {'SizeMts': sizemts, 'PiceUser': priceuser, 'locariouser': locariouser, 'message': valuemessage, 'data': data}
 
-def str_value(s):
-    return str('Distrito: '+s)
+@app.get("/price_same_model/{sizemts}/{location}/{priceuser}")
+def read_calculate(sizemts: int, location: str, priceuser: int, locariouser: Union[str, None] = None):
+    valuemessage = ''
+    x_train, y_train, z_train = load_data(location)
+    m = x_train.shape[0]
+    nro = 0
+    data = {}
+    data['predictions'] = [] 
+    for i in range(m):        
+        if(x_train[i] == sizemts):
+            nro = nro + 1 
+            data['predictions'].append({
+                'inmueble': nro,
+                'size': x_train[i],
+                'price': f"$ {format(y_train[i] + b,'0,.2f')}",
+                #'price': f"$ {format(w * y_train[i] + b,'0,.2f')}",
+                'recommended': True,
+                'location': read_location(z_train[i])}) 
+            valuemessage = 'completed process'
+        #else:
+            #valuemessage = 'Sin valores encontrados'
+    with open('modelorl.json', 'w') as file:
+        json.dump(data, file, indent=4)
+    return {'SizeMts': sizemts, 'PiceUser': priceuser, 'locariouser': locariouser, 'message': valuemessage, 'data': data}
+
+
+"""
+def price_same(sizemts):    
+    m = x_train.shape[0]
+    nro = 0
+    data = {}
+    data['predictions'] = [] 
+    for i in range(m):
+        nro = nro + 1 
+        if(x_train[i] == sizemts):
+            data['predictions'].append({
+                'inmueble': nro,
+                'size': x_train[i],
+                'price': f"$ {format(w * y_train[i] + b,'0,.2f')}",
+                'recommended': True,
+                'location': read_location(z_train[i])})
+    return data
+
+    ---------
+
+    data['predictions'].append({
+        'inmueble': 1,
+        'bedrooms': x_house[1],
+        'floors': x_house[2],
+        'age': edad,        
+        'recommended': True}) 
+"""
+
+# load house data.
+Xm_train, ym_train, zm_train = load_house_data()
+
+def zscore_normalize_features(X):    
+    # find the mean of each column/feature
+    mu     = np.mean(X, axis=0)                 # mu will have shape (n,)    
+    sigma  = np.std(X, axis=0)                  # sigma will have shape (n,)    
+    X_norm = (X - mu) / sigma      
+
+    return (X_norm, mu, sigma)
+
+# First, normalize out example.
+@app.get("/price_house_model/{sizemts}/{location}/{age}")
+def read_calculate(sizemts: int, location: int, age: int):
+    #pie_cuadrado = 10.764
+
+    data = {}
+    data['characteristic'] = []
+
+    #p = pie_cuadrado()#10.764
+    sqft, iterations, alpha = pie_cuadrado()#10.764
+    
+    if age > 10:
+         valuemessage = 'construcción del inmueble no mayor a 10 años'
+    else:
+
+        if zm_train[5] == location:
+
+            Xm_norm, Xm_mu, Xm_sigma = zscore_normalize_features(Xm_train)
+            wm_norm, bm_norm, hist = run_gradient_descent(Xm_norm, ym_train, iterations, alpha)
+            
+            mts = sizemts
+            mts_cuadrados = mts * sqft
+
+            #construccion = random.randint(5, 20)        
+            x_house = np.array([mts_cuadrados, 4, 2, age])    
+            x_house_norm = (x_house - Xm_mu) / Xm_sigma    
+            x_house_predict = np.dot(x_house_norm, wm_norm) + bm_norm
+            x_price = x_house_predict * 1000
+
+            data['characteristic'].append(
+                {
+                'inmueble': 1,
+                'size': sizemts,
+                'price': f"$ {format(x_price,'0,.2f')}",
+                'bedrooms': x_house[1],
+                'floors': x_house[2],
+                'age': age,            
+                'location': read_location(location)
+                })
+
+            valuemessage = 'completed process' 
+        else:        
+            valuemessage = 'dataset no definido para {}'.format(read_location(location))
+
+    return {'SizeMts': sizemts, 'message': valuemessage, 'data': data}    
--- prediction-modelo-one/utils.py
+++ prediction-modelo-one/utils.py
@@ -1,8 +1,169 @@
 import numpy as np
+import os# para manipular rutas
 
-def load_data():
-    data = np.loadtxt("data/MRL01/dataset.txt", delimiter=',')
-    #data = np.loadtxt("data/trainingdata.txt", delimiter=',')
-    X = data[:,0]
-    y = data[:,1]
-    return X, y
+def weight_bias():
+    w = 3050
+    b = 300
+    #w = 1200
+    #b = 300
+    return w, b
+
+# Load dataset according to locations
+# function start
+def load_data(locations: str):
+    location = locations  
+    values = ''
+
+    file_path = 'data/MRL01/locations/dataset{}.txt'.format(location)
+
+    if os.path.exists(file_path) == True:
+        data = np.loadtxt(file_path, delimiter=',')
+    
+        x = data[:,0]
+        y = data[:,1]
+        z = data[:,2]
+
+        values = x, y, z
+    else:
+        values = 'Dataset no definido'
+
+    return values
+# function end
+
+def load_location():
+    data = np.loadtxt("data/LOCATION/location-lima-metropolitana.txt", delimiter=',')    
+    id = data[:,0]
+    lo = data[:,1]
+    return id, lo
+
+def percent_location(code):
+    if (code == 1):
+        values = 0.040
+    elif(code == 2):
+        values = 0.020
+    elif(code == 3):
+        values = 0.012
+    elif(code == 4):
+        values = 0.050
+    elif(code == 5):
+        values = 0.040
+    elif(code == 6):
+        values = 0.037    
+    
+    return values
+
+def read_location(code: int):
+    if (code == 1):
+        values = 'Cercado de Lima'
+    elif(code == 2):
+        values = 'Ancón'
+    elif(code == 3):
+        values = 'Ate'
+    elif(code == 4):
+        values = 'Barranco'
+    elif(code == 5):
+        values = 'Breña'
+    elif(code == 6):
+        values = 'Carabayllo'
+    elif(code == 7):
+        values = 'Comas'
+    elif(code == 8):
+        values = 'Chaclacayo'
+    elif(code == 9):
+        values = 'Chorrillos'
+    elif(code == 10):
+        values = 'El Agustino'
+    elif(code == 11):
+        values = 'Jesús María'
+    elif(code == 12):
+        values = 'La Molina'
+    elif(code == 13):
+        values = 'La Victoria'
+    elif(code == 14):
+        values = 'Lince'
+    elif(code == 15):
+        values = 'Lurigancho'
+    elif(code == 16):
+        values = 'Lurín'
+    elif(code == 17):
+        values = 'Magdalena'
+    elif(code == 18):
+        values = 'Miraflores'
+    elif(code == 19):
+        values = 'Pachacamac'
+    elif(code == 20):
+        values = 'Pucusana'
+    elif(code == 21):
+        values = 'Pueblo Libre'
+    elif(code == 22):
+        values = 'Puente Piedra'
+    elif(code == 23):
+        values = 'Punta Negra'
+    elif(code == 24):
+        values = 'Punta Hermosa'
+    elif(code == 25):
+        values = 'Rímac'
+    elif(code == 26):
+        values = 'San Bartolo'
+    elif(code == 27):
+        values = 'San Isidro'
+    elif(code == 28):
+        values = 'Independencia'
+    elif(code == 29):
+        values = 'San Juan de Miraflores'
+    elif(code == 30):
+        values = 'San Luis'
+    elif(code == 31):
+        values = 'San Martín de Porres'
+    elif(code == 32):
+        values = 'San Miguel'
+    elif(code == 33):
+        values = 'Santiago de Surco'
+    elif(code == 34):
+        values = 'Surquillo'
+    elif(code == 35):
+        values = 'Villa María del Triunfo'
+    elif(code == 36):
+        values = 'San Juan de Lurigancho'
+    elif(code == 37):
+        values = 'Santa María del Mar'
+    elif(code == 38):
+        values = 'Santa Rosa'
+    elif(code == 39):
+        values = 'Los Olivos'
+    elif(code == 40):
+        values = 'Cieneguilla'
+    elif(code == 41):
+        values = 'San Borja'
+    elif(code == 42):
+        values = 'Villa el Salvador'
+    elif(code == 43):
+        values = 'Santa Anita'
+    elif(code == 44):
+        values = 'Callao'
+    elif(code == 45):
+        values = 'Bellavista'
+    elif(code == 46):
+        values = 'Carmen de la Legua'
+    elif(code == 47):
+        values = 'La Perla'
+    elif(code == 48):
+        values = 'La Punta'
+    elif(code == 49):
+        values = 'Ventanilla'
+    else:
+        values = 'Sin Identificar'
+    return values
+    
+def price_same(x_train, y_train, z_train, sizemts: int):
+    m = x_train.shape[0]
+    data = []
+    for i in range(m):
+        if(x_train[i] == sizemts):
+            data.append({x_train[i], y_train[i], z_train[i]})
+    return data 
+
+def is_list_empty(list):
+    if len(list) != 0:        
+        return True    
+    return False
